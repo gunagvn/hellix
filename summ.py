284318c6ecca158868910049e5ea956d506e9689
Gunajeevan M <gunagvn@gmail.com>
2025-10-25 09:38:12 +0530

    new

---
+++ summ.py
@@ -0,0 +1,8 @@
+#add 3 numbers to find sum and average
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+sum = a + b + c
+average = sum / 3
+print("Sum is:", sum)
+print("Average is:", average)
