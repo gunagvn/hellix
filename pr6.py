97681f103a011e9083122062e9e4484446551d5e
Gunajeevan M <gunagvn@gmail.com>
2025-10-25 17:31:06 +0530

    problem6

---
+++ pr6.py
@@ -0,0 +1,10 @@
+val = []
+while True:
+    num = int(input("Enter numbers:"))
+    if num < 0 :
+        print("Negative number")
+        break
+    val.append(num)
+print("the positive numbers",val)  
+    
+    
