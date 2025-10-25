b5cd65117f763391a9927032ce8c14184f441cde
Gunajeevan M <gunagvn@gmail.com>
2025-10-25 23:35:48 +0530

    changes

--- prim.py
+++ prim.py
@@ -1,7 +1,7 @@
-varr = []
+varr = [] #Creating a new empty list to append values
 for i in range(10,101):
-    if i%2 != 0 :
-        varr.append(i)
+    if i%2 != 0 : #This is the condition to check Whether the number is prime or not
+        varr.append(i) #This syntax helps to append the values to that empty new list
         
 print("Prime numbers:",varr)       
      
