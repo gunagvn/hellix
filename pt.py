76a15f894d2ea5fed06f5146bdd7e01f02cd3242
Gunajeevan M <gunagvn@gmail.com>
2025-10-25 09:44:00 +0530

    added last row

--- pt.py
+++ pt.py
@@ -4,5 +4,6 @@ n = int(input("Enter number of rows: "))
 for i in range(1, n+1):
     #inner loop is for printing stars
     for j in range(1, i+1):
-        print("*", end="")
+        print("*", end=" ")
+    print()  # for new line after each row
     
